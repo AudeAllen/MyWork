e12e2db830e5834b5cc1f595b054ed92c34848d6
Audrey Allen <audrey.allen@hse.ie>
2023-02-13 18:50:45 +0000

    Add

---
+++ Week 03/lab3.2.1-round.py
@@ -0,0 +1,7 @@
+# Rounds a number.
+
+# Author: Audrey Allen
+
+numberToRound = float(input("enter a float number:"))
+roundedNumber = round(numberToRound)
+print ( '{} rounded is {}'.format(numberToRound,roundedNumber))
